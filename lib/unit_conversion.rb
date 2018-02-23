4d44232ada53d2d1e65a0a759b91ddf2893d0061
Brian Buchalter <bal711@gmail.com>
2018-02-23 10:37:38 -0700

    Division, multiplication of conversions
    This will allow us to evaluate expressions of conversions.

--- lib/unit_conversion.rb
+++ lib/unit_conversion.rb
@@ -48,5 +48,29 @@ module UnitConversion
       @unit_name = unit_name
       @multiplication_factor = multiplication_factor
     end
+
+    def ==(other)
+      unit_name == other.unit_name && multiplication_factor == other.multiplication_factor
+    end
+
+    def /(operand)
+      new_unit = "#{unit_name}/#{operand.unit_name}"
+      new_factor = (multiplication_factor / operand.multiplication_factor.to_f).round(14)
+
+      self.class.new(
+        unit_name: new_unit,
+        multiplication_factor: new_factor
+      )
+    end
+
+    def *(operand)
+      new_unit = "#{unit_name}*#{operand.unit_name}"
+      new_factor = (multiplication_factor * operand.multiplication_factor.to_f).round(14)
+
+      self.class.new(
+        unit_name: new_unit,
+        multiplication_factor: new_factor
+      )
+    end
   end
 end
